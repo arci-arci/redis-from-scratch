1da0c151e7c6719f52a8f81bf94f0bccb93bdb83
Stefano Labianca <lstefano@proton.me>
2025-10-05 20:53:23 +0200

    some corrections on code validation

--- client.py
+++ client.py
@@ -1,29 +1,30 @@
-import socket
+from socket import socket, AF_INET, SOCK_STREAM
 from commons import HOST, PORT, BUFFER_SIZE
 from commands import ValidateCommand, show_help
 
 if __name__ == "__main__":
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket(AF_INET, SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         validator = ValidateCommand()
 
         while True:
             user_input = input("repy> ").strip()
+            command_type: str = user_input.split(" ")[0]
 
-            if not validator.is_a_command(user_input): 
+            if not validator.is_a_command(command_type):  
                 print(f"Command {user_input} is not a valid command")
                 continue
-            
-            if user_input == "HELP":
-                show_help()
-            
 
+            if command_type == "HELP":
+                show_help()
+                continue
+            
             raw_data = bytes(user_input, "utf-8")
             s.sendall(raw_data)
 
             raw_data: bytes = s.recv(BUFFER_SIZE)
             data: str = raw_data.decode("utf-8")
-            print(f"data: {data}")
+            print(data)
 
 
             if data == "EXIT":
