073b611a57b972245d91fb2816eabfa6632ef9ef
Stefano Labianca <lstefano@proton.me>
2025-10-11 15:58:20 +0200

    now you can use a .env file to store the host ip address and port number

--- src/utility/commons.py
+++ src/utility/commons.py
@@ -1,3 +1,12 @@
-HOST: str = "127.0.0.1"  
-PORT: int = 6969
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+_DEFAULT_HOST = "127.0.0.1"
+_DEFAULT_PORT = "6969"
+
+HOST: str = os.environ.get("RE_HOST") or  _DEFAULT_HOST
+PORT: int = int(os.environ.get("RE_PORT_NUMBER") or _DEFAULT_PORT)
 BUFFER_SIZE: int = 1024
+
